71786f3c2b6dfba92ae965c81741fcba3eebb9ef
chocolateboy <chocolate@cpan.org>
2025-08-01 04:14:11 +0100

    Fix the trigger documentation
    document the (optional) third argument if the new value differs from the
    old value

--- lib/Mouse.pm
+++ lib/Mouse.pm
@@ -352,7 +352,7 @@ Use of this feature requires L<Scalar::Util>!
 
 =item C<< trigger => CodeRef >>
 
-Any time the attribute's value is set (either through the accessor or the constructor), the trigger is called on it. The trigger receives as arguments the instance, and the new value.
+Any time the attribute's value is set (either through the accessor or the constructor), the trigger is called on it. The trigger receives as arguments the instance, the new value, and optionally the old value if it differs from the new value.
 
 =item C<< builder => Str >>
 
